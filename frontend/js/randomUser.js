ea7afa3aa55e1330b03a9162ff483648dd16694a
varshi0829 <adapasreevarshitha@gmail.com>
2026-06-20 10:32:24 +0530

    Random User feature added

---
+++ frontend/js/randomUser.js
@@ -0,0 +1,19 @@
+function randomUser(){
+    fetch("https://randomuser.me/api/")
+
+    .then(function(res)
+    {
+        return res.json();
+    })
+    .then(function(data){
+        var user = data.results[0];
+        var fullName = user.name.title + " " + user.name.first + " " + user.name.last;
+            document.getElementById("user-image").src=user.picture.large;
+            document.getElementById("user-name").innerText=fullName;
+            document.getElementById("user-gender").innerText=user.gender;
+    })
+
+    .catch(function(err){
+        console.log("Error: " + err);
+    });
+}
